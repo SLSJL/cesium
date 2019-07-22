a9f8a45a3d5330767d2b63028c72f150c803266a
mrraindrop <350916525@qq.com>
2019-07-22 19:50:17 +0800

    add map view

---
+++ 1-code/src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div id="cesium-credit"></div>
+    <div class="home" id="home"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {},
+  data() {
+    return {
+      map: null,
+      viewerOpts: {
+        animation: false,
+        timeline: false,
+        geocoder: false,
+        fullscreenButton: false,
+        sceneModePicker: false,
+        navigationHelpButton: false,
+        homeButton: false,
+        baseLayerPicker: false,
+        navigationInstructionsInitiallyVisible: false,
+        creditContainer: "cesium-credit"
+      }
+    };
+  },
+  mounted() {
+    this.map = this.$utils.map.initCesium("home", this.viewerOpts);
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.home {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  background-color: #eee;
+}
+</style>
